7f16ec2e7ce286517f668d31c803e3c9e49a13e5
jrc452 <jairoreyescalvario@gmail.com>
2025-11-03 23:26:21 +0100

    Update

--- compra/src/main/java/com/miapp/compra/repository/CategoriaRepository.java
+++ compra/src/main/java/com/miapp/compra/repository/CategoriaRepository.java
@@ -1,9 +1,25 @@
 package com.miapp.compra.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.miapp.compra.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    List<Categoria> findByNombreContainingAndDescripcionContaining(String nombre, String descripcion);
+
+    List<Categoria> findByDescripcionContainingIgnoreCase(String descripcion);
+
+    List<Categoria> findAllByOrderByNombreAsc();
+
+    Long countByNombreContaining(String texto);
+
+    Categoria findTopByOrderByIdCategoriaDesc();
+
+    @Query("SELECT c FROM Categoria c WHERE SIZE(c.productos) > :minimo")
+    List<Categoria> findByMinimoProductos(int minimo);
 
 }
--- compra/src/main/java/com/miapp/compra/repository/ProductoRepository.java
+++ compra/src/main/java/com/miapp/compra/repository/ProductoRepository.java
@@ -1,9 +1,21 @@
 package com.miapp.compra.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.miapp.compra.model.Producto;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    void deleteByStockLessThan(int cantidad);
+
+    void deleteByCategoriaIdCategoria(Long idCategoria);
+
+    List<Producto> findByPrecioBetween(double min, double max);
+
+    @Query("SELECT p FROM Producto p WHERE p.categoria.idCategoria = :idCategoria AND p.stock > 0")
+    List<Producto> findDisponiblesPorCategoria(Long idCategoria);
 
 }
