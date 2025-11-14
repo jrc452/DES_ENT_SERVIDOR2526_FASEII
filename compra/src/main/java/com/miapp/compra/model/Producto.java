3278f8e0860067ecea502a40b936137f4a6e1c52
jrc452 <jairoreyescalvario@gmail.com>
2025-11-14 12:57:22 +0100

    Update

--- compra/src/main/java/com/miapp/compra/model/Producto.java
+++ compra/src/main/java/com/miapp/compra/model/Producto.java
@@ -7,9 +7,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
-@SuppressWarnings("unused")
 @Entity
+@Data
 @Table(name = "productos")
 public class Producto {
     @Id
